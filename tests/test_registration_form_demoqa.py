0b7493a123d7c9cc71446cba8930950f09b9eac2
AleksSh <Coluchy@yandex.ru>
2024-09-22 01:05:53 +0600

    first commit

--- tests/test_registration_form_demoqa.py
+++ tests/test_registration_form_demoqa.py
@@ -1,28 +1,42 @@
-from selene import browser, command, have
+from pathlib import Path
+
+import allure
+from allure_commons.types import Severity
+from selene import have, command
 import os
 
 
-def test_registration_form_demoqa():
-    browser.open('/automation-practice-form')
+@allure.tag("web")
+@allure.severity(Severity.MINOR)
+@allure.label("owner", "Аркадий Укупник")
+@allure.feature("Регистрация")
+@allure.story("Проверка заполнения формы регистрации")
+def test_registration_form_demoqa(setup_browser):
+    browser = setup_browser
 
-    browser.element('#firstName').type('Coluchy')
-    browser.element('#lastName').type('Aleksandr')
-    browser.element('#userEmail').type('AC@ya.com')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element("#userNumber").type('4455667788')
-    browser.element('#subjectsInput').type('computer').press_enter()
-    browser.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
-    browser.element('.react-datepicker__month-select').click().element('[value="6"]').click()
-    browser.element('.react-datepicker__year-select').click().element('[value="1986"]').click()
-    browser.element('.react-datepicker__day--008').click()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('../8.png'))
-    browser.element('#currentAddress').type('India').press_enter()
-    browser.element("#react-select-3-input").type("Rajasthan").press_enter()
-    browser.element("#react-select-4-input").type("Jaiselmer").press_enter()
-    browser.element('#submit').press_enter()
-    browser.element("#example-modal-sizes-title-lg").should(have.text('Thanks for submitting the form'))
+    with allure.step("Open registrations form"):
+        browser.open('/automation-practice-form')
 
-    browser.element('.table').all('td').even.should(
-        have.exact_texts('Coluchy Aleksandr', 'AC@ya.com', 'Male', '4455667788', '08 July,1986', 'Computer Science',
-                         'Sports', '8.png', 'India', 'Rajasthan Jaiselmer'))
+    with allure.step("Fill form"):
+        browser.element('#firstName').type('Coluchy')
+        browser.element('#lastName').type('Aleksandr')
+        browser.element('#userEmail').type('AC@ya.com')
+        browser.element('[for="gender-radio-1"]').click()
+        browser.element("#userNumber").type('4455667788')
+        browser.element('#subjectsInput').type('computer').press_enter()
+        browser.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
+        browser.element('.react-datepicker__month-select').click().element('[value="6"]').click()
+        browser.element('.react-datepicker__year-select').click().element('[value="1986"]').click()
+        browser.element('.react-datepicker__day--008').click()
+        browser.element('[for="hobbies-checkbox-1"]').click()
+        browser.element('#uploadPicture').send_keys(str(Path(__file__).parent.parent.joinpath(f'resources/8.png')))
+        browser.element('#currentAddress').type('India').press_enter()
+        browser.element("#react-select-3-input").type("Rajasthan").press_enter()
+        browser.element("#react-select-4-input").type("Jaiselmer").press_enter()
+        browser.element('#submit').press_enter()
+        browser.element("#example-modal-sizes-title-lg").should(have.text('Thanks for submitting the form'))
+
+    with allure.step("Check form results"):
+        browser.element('.table').all('td').even.should(
+            have.exact_texts('Coluchy Aleksandr', 'AC@ya.com', 'Male', '4455667788', '08 July,1986', 'Computer Science',
+                             'Sports', '8.png', 'India', 'Rajasthan Jaiselmer'))
