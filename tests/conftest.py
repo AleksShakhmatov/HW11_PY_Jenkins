5fd61a24914b17b861102b52d335c652e7db776b
AleksSh <Coluchy@yandex.ru>
2024-09-23 23:50:52 +0600

    first commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,7 +43,7 @@ def browser_management(request):
     password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
