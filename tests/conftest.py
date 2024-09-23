b33f6da455b2c66ed4f86aee7ef8762f9e63efbf
AleksSh <Coluchy@yandex.ru>
2024-09-24 01:17:18 +0600

    first commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,7 @@ def setup_browser(request):
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
+        "browserName": "Chrome",
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
