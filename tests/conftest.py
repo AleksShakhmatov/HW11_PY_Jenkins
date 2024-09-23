375d826bc69902277ecf2d7bfd178db29b200ace
AleksSh <Coluchy@yandex.ru>
2024-09-24 00:03:06 +0600

    first commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,14 @@
 import pytest
-from selene import browser
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
 from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
-def setting_browser(request):
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-
+@pytest.fixture(scope='function')
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -21,14 +18,13 @@ def setting_browser(request):
             "enableVideo": True
         }
     }
-
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
+        options=options
+    )
 
-    browser.config.driver = driver
-
+    browser = Browser(Config(driver))
     yield browser
 
     attach.add_screenshot(browser)
